82838542a68d567835b15a835e1d73d56b02cf9b
JUKI <jyukipann@yahoo.co.jp>
2022-06-16 22:59:46 +0900

    sobel filter

--- palmutil.py
+++ palmutil.py
@@ -2,6 +2,11 @@ import mediapipe as mp
 import cv2
 import numpy as np
 
+def sobel_filter(gray):
+    gray_x = cv2.Sobel(gray, cv2.CV_32F, 1, 0, ksize=3)
+    gray_y = cv2.Sobel(gray, cv2.CV_32F, 0, 1, ksize=3)
+    return np.sqrt(np.square(gray_x) + np.square(gray_y)).astype(np.uint8)
+
 if __name__ == "__main__":
     org_img = cv2.imread(r"media\myLeftHand.jpg")
     org_img_h,org_img_w,_ = org_img.shape
@@ -29,11 +34,21 @@ if __name__ == "__main__":
     ]
     palm_img_rect = np.array(palm_img_rect).astype(int)
     xmin,ymin,xmax,ymax = palm_img_rect
-    margin = 100
-    xmin -= margin
-    np.clip(xmin-margin, 2, None)
+    margin = 0
+    xmin = np.clip(xmin-margin, 0, org_img_w)
+    xmax = np.clip(xmax+margin, 0, org_img_w)
+    ymin = np.clip(ymin-margin, 0, org_img_h)
+    ymax = np.clip(ymax+margin, 0, org_img_h)
     print(palm_img_rect)
     palm_org_img = org_img[ymin:ymax,xmin:xmax,:]
+    # palm_nlm_img = cv2.fastNlMeansDenoisingColored(palm_org_img,None,10,10,7,21)
+
+    gray = cv2.cvtColor(palm_org_img,cv2.COLOR_BGR2GRAY)
+    sobel_img = sobel_filter(gray)
+    # nlm_sobel_img = sobel_filter(cv2.cvtColor(palm_nlm_img,cv2.COLOR_BGR2GRAY))
+    cv2.imshow("sobel_img",sobel_img)
+    # cv2.imshow("nlm_sobel_img",nlm_sobel_img)
     cv2.imshow("palm_org_img",palm_org_img)
+    # cv2.imshow("palm_nlm_img",palm_nlm_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
